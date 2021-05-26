874145f50ca1d6da39f799d475d27f8b7a3864eb
A_Artsemyeu <Nika777fm@tut.by>
2021-05-26 22:37:41 +0300

    Task03

--- src/by/epam/unit03/main/Task03.java
+++ src/by/epam/unit03/main/Task03.java
@@ -1,5 +1,14 @@
 package by.epam.unit03.main;
 
 public class Task03 {
-
+	public static void main(String[] args) {
+		int summa = 0;
+		int result = 1;
+		
+		for(int i = 1; i <= 10; i++) {
+			summa = summa + i;
+			result = result * summa;
+		}
+		System.out.println("Result is " + result);
+	}
 }
