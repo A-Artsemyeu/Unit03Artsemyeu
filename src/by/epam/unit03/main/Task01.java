d6b084ff0fb8fc7e9daecf75dbcf897d97305cb9
A_Artsemyeu <Nika777fm@tut.by>
2021-05-26 21:31:30 +0300

    Task01

---
+++ src/by/epam/unit03/main/Task01.java
@@ -0,0 +1,14 @@
+package by.epam.unit03.main;
+
+public class Task01 {
+	public static void main (String[] args) {
+		int a = 0;
+		int b = 3;
+		
+		while (a <= 10) {
+			int rezult = a * b;
+			System.out.println(a + " * 3 = " + rezult );
+			a++;
+		}
+	}
+}
