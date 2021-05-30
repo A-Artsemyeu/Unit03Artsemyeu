810d42901e1c6ae499dedbf968c8e1d8ab1a3d8b
A_Artsemyeu <Nika777fm@tut.by>
2021-05-31 00:41:22 +0300

    Task06

--- src/by/epam/unit03/main/Task06.java
+++ src/by/epam/unit03/main/Task06.java
@@ -1,15 +1,15 @@
 package by.epam.unit03.main;
 
 public class Task06 {
-	public static void main (String[] args) {
-		
+	public static void main(String[] args) {
+
 		int numberFirst, numberSecond, number;
-		
+
 		int a = 1657093;
 		int b = 2835891;
-		
+
 		for (int i = a; i > 0; i = i / 10) {
-			numberFirst =  i % 10;
+			numberFirst = i % 10;
 			for (int j = b; j > 0; j = j / 10) {
 				numberSecond = j % 10;
 				if (numberFirst == numberSecond) {
