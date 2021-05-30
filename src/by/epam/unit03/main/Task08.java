97353779a9a9b1679826b76c17fcb25d1bb352f7
A_Artsemyeu <Nika777fm@tut.by>
2021-05-31 00:25:33 +0300

    Task08

---
+++ src/by/epam/unit03/main/Task08.java
@@ -0,0 +1,76 @@
+package by.epam.unit03.main;
+
+import java.util.Scanner;
+
+public class Task08 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		double x, y, z;
+		char operationOfSign;
+		
+		while (true) {
+
+		System.out.print("Введите операнд X: ");
+		while(!scanner.hasNextDouble()){
+			scanner.next();
+			System.out.println("Вы ввели не число.");
+			System.out.print("Введите операнд X: ");
+			}
+		x = scanner.nextDouble();
+		
+		System.out.print("Введите операнд Y: ");
+		while(!scanner.hasNextDouble()){
+			scanner.next();
+			System.out.println("Вы ввели не число.");
+			System.out.print("Введите операнд Y: ");
+			}
+		y = scanner.nextDouble();
+		
+		System.out.print("Введите знак операции (+, –, /, *): ");
+
+		operationOfSign = scanner.next().charAt(0);
+		
+		switch (operationOfSign){
+		
+		case '+':
+			z = x + y;
+			System.out.println("z = "+ z);
+			break;
+		case '-':
+			z = x - y;
+			System.out.println("z = "+ z);
+			break;
+		case '*':
+			z = x * y;
+			System.out.println("z = "+ z);
+			break;
+		case '/':
+			if (y == 0) {
+				System.out.println("Деление на ноль недопустимо. Повторите ввод.");
+				break;
+			} else {
+			z = x / y;
+			System.out.println("z = "+ z);
+			}
+			break;
+		default:
+            System.out.println("Вы ввели не (+, –, /, *). Повторите ввод.");
+            continue;            
+		}	
+		System.out.print("Введите # для выхода из программы либо Enter для продолжения: ");	
+		if (scanner.hasNextLine()) {
+			scanner.nextLine();
+			if (scanner.nextLine().equals("#")) {
+				break;
+				}
+		}else if (scanner.nextLine().equals ("")) {
+			continue;
+			}
+
+		}
+		scanner.close();
+	}
+
+}
